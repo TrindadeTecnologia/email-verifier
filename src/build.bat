9a8fa36524e41ee74d7b96d9630adf5452ce4750
oliveiracdz <oliveiracdz@hotmail.com>
2016-12-23 13:26:25 -0200

    Arquivos de configuração da build MyGet.

---
+++ src/build.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Trindade.EmailVerifier.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net45
+
+%nuget% pack "src\Trindade.EmailVerifier.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
