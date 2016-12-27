10e528234cf53d7d701dc2d020ca8bc631df5ed8
Daniel Oliveira <oliveiracdz@hotmail.com>
2016-12-27 15:01:22 -0200

    Update build.bat

--- src/build.bat
+++ src/build.bat
@@ -1,28 +1,22 @@
-@echo off
-
+@echo Off
 set config=%1
 if "%config%" == "" (
    set config=Release
 )
- 
-set version=1.0.0
+
+set version=
 if not "%PackageVersion%" == "" (
-   set version=%PackageVersion%
+   set version=-Version %PackageVersion%
 )
 
-set nuget=
-if "%nuget%" == "" (
-	set nuget=nuget
-)
-
-REM Restore Packages
 %nuget% restore src\Trindade.EmailVerifier.sln
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Trindade.EmailVerifier.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Trindade.EmailVerifier.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
+REM Package
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net45
 
-%nuget% pack "src\Trindade.EmailVerifier.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+cmd /c %nuget% pack "src\Trindade.EmailVerifier.nuspec" -symbols -o Build -p Configuration=%config% %version%
