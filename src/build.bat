d760b00d050d876e7387b498d0db01cb5157e73a
Daniel Oliveira <oliveiracdz@hotmail.com>
2016-12-28 11:28:45 -0200

    Update build.bat

--- src/build.bat
+++ src/build.bat
@@ -12,7 +12,7 @@ if not "%PackageVersion%" == "" (
 %nuget% restore src\Trindade.EmailVerifier.sln
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Trindade.EmailVerifier.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /tv:14.0 /p:GenerateBuildInfoConfigFile=false /p:VisualStudioVersion=14.0
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Trindade.EmailVerifier.sln /tv:14.0 /p:GenerateBuildInfoConfigFile=false /p:VisualStudioVersion=14.0
 
 REM Package
 mkdir Build
