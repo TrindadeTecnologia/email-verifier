1c08b65fc6990bc258a02102cb9a79047f5d5e6a
Daniel Oliveira <oliveiracdz@hotmail.com>
2017-01-18 14:56:34 -0200

    Update build.cmd

--- src/build.cmd
+++ src/build.cmd
@@ -1,2 +1,2 @@
-nuget restore src/Trindade.EmailVerifier.sln
+nuget restore src\Trindade.EmailVerifier.sln
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Trindade.EmailVerifier.sln /p:Configuration="Release" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /tv:14.0 /p:GenerateBuildInfoConfigFile=false /p:VisualStudioVersion=14.0
